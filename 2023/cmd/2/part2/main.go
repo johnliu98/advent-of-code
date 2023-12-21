623075d88a5aa1b0d11a1056e67a16bac2de4470
John Liu <johnliu@kth.se>
2023-12-21 14:53:46 +0100

    Day 2: Cube Conundrum - Part 2

---
+++ 2023/cmd/2/part2/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/johnliu98/advent-of-code/2023/internal/read"
+)
+
+func main() {
+	input := read.ReadLines("./cmd/2/input.txt")
+
+	var ans int
+	for _, in := range input {
+		colors := map[string]int{}
+		for _, s := range read.Sets(in) {
+			for _, cube := range strings.Split(s, ",") {
+				x := strings.Split(strings.Trim(cube, " "), " ")
+
+				n, err := strconv.Atoi(x[0])
+				if err != nil {
+					panic(err)
+				}
+				color := x[1]
+
+				colors[color] = max(colors[color], n)
+			}
+		}
+
+		ans += colors["red"] * colors["green"] * colors["blue"]
+	}
+
+	fmt.Println("Answer: ", ans)
+}
